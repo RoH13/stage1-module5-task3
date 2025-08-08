268cb241e8f9077cf435ab014bbd8df82f1d86a1
raman <r4m4n911@gmail.com>
2025-08-08 22:30:54 +0300

    gf

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -2,26 +2,33 @@ package com.epam.mjc;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+       return list.stream().filter(t -> t % 2 == 0).toList();
+        // throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+       return list.stream().map(x -> x.toUpperCase()).toList();
+        // throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().max(Integer::compare);
+        //throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().flatMap(x -> x.stream().min(Integer::compare).stream()).min(Integer::compare);
+        //throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+       return list.stream().reduce((x,y) -> x + y).get();
+        // throw new UnsupportedOperationException("You should implement this method.");
     }
+    
 }
